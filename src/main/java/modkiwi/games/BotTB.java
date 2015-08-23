84585b52994786e8683255c22e6b6a1431d70c6a
Kara Felix <karairenefelix@gmail.com>
2015-08-23 03:20:00 -0400

    BotTB.java: implemented TimeBomb bot

--- src/main/java/modkiwi/games/BotTB.java
+++ src/main/java/modkiwi/games/BotTB.java
@@ -6,6 +6,7 @@ import modkiwi.util.Logger;
 import modkiwi.util.Utils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +14,10 @@ import java.util.List;
 public class BotTB extends GameBot
 {
     private static final Logger LOGGER = new Logger(BotTB.class);
+
+    private int NoP;
+    private String[][] hands;
+    private int success, safe, turn, round;
 
     protected BotTB(GameInfo game) throws IOException
     {
@@ -22,32 +27,392 @@ public class BotTB extends GameBot
     @Override
     public void createGame()
     {
+        NoP = game.getPlayers().size();
+        int evil, good;
+        if (NoP <= 7)
+            evil = 2;
+        else
+            evil = 3;
+        good = NoP - evil;
+        if (NoP == 4)
+            good++;
+
+        List<String> roles = new ArrayList<String>(good + evil);
+        for (int i = 0; i < good; i++)
+            roles.add("good");
+        for (int i = 0; i < evil; i++)
+            roles.add("evil");
+
+        Collections.shuffle(roles);
+        game.setDataProperty("roles", roles);
+
+        List<Integer> initdeck = new ArrayList<Integer>(5 * NoP);
+        initdeck.add(-1);
+        for (int i = 0; i < NoP; i++)
+            initdeck.add(NoP - i);
+        while (initdeck.size() < 5 * NoP)
+            initdeck.add(0);
+
+        List<Integer> deck;
+
+        for (int i = 0; i < 4; i++)
+        {
+            deck = new ArrayList<Integer>(initdeck.subList(0, NoP * (5 - i)));
+            Collections.shuffle(deck);
+            game.setDataProperty("round" + (i + 1) +  "deck", deck);
+        }
+    }
+
+    private void sendRoles()
+    {
+        List<String> roles = (List<String>)game.getDataProperty("roles");
+        String player, role, subject, message, modMessage;
+        String goodMessage = "You are a virtuous member of the SWAT team trying to defuse the bomb.";
+        String evilMessage = "You are a neferious evil terrorist trying to make the bomb blow up.";
+        modMessage = "";
+        for (int i = 0; i < NoP; i++)
+        {
+            player = players[i];
+            role = roles.get(i);
+
+            modMessage += player + " - " + role + "\n";
+
+            subject = game.getPrefix() + " - YOU ARE " + role.toUpperCase();
+            if (role.equals("good"))
+                message = goodMessage;
+            else
+                message = evilMessage;
+
+            try
+            {
+                helper.geekmail(player, subject, message);
+            }
+            catch (IOException e)
+            {
+                LOGGER.throwing("sendRoles()", e);
+            }
+        }
+
+        subject = game.getPrefix() + " - Player Roles";
+        try
+        {
+            helper.geekmail(game.getNonPlayerMods(), subject, modMessage);
+        }
+        catch (IOException e)
+        {
+            LOGGER.throwing("sendRoles()", e);
+        }
+    }
+
+    private void sendHands()
+    {
+        String player, message, modMessage;
+        String subject = game.getPrefix() + " - Round " + round + " Hand";
+        final int nameLen = 20;
+        int success, safe, boom;
+        modMessage = "[c]";
+        for (int i = 0; i < NoP; i++)
+        {
+            player = players[i];
+
+            success = 0;
+            safe = 0;
+            boom = 0;
+
+            modMessage += String.format("%-" + nameLen + "s", player);
+
+            for (String card : hands[i])
+            {
+                if (card == null)
+                {
+                    modMessage += "  ";
+                    LOGGER.warning("Null card found in hand of %s.", player);
+                }
+                else if (card.equals("success"))
+                {
+                    success++;
+                    modMessage += " [b]g{S}g[/b]";
+                }
+                else if (card.equals("boom"))
+                {
+                    boom++;
+                    modMessage += " [b]r{B}r[/b]";
+                }
+                else
+                {
+                    safe++;
+                    modMessage += " -";
+                }
+            }
+
+            modMessage += "\n";
+            message = "Your round " + round + " hand contains:";
+            if (success > 0)
+                message += "\ng{" + success + " x SUCCESS}g";
+            if (safe > 0)
+                message += "\nb{" + safe + " x SAFE}b";
+            if (boom > 0)
+                message += "\nr{" + boom + " x BOOM}r";
+
+            try
+            {
+                helper.geekmail(player, subject, message);
+            }
+            catch (IOException e)
+            {
+                LOGGER.throwing("sendHands()", e);
+            }
+        }
+
+        subject = game.getPrefix() + " - Round " + round + " Hands";
+        try
+        {
+            helper.geekmail(game.getNonPlayerMods(), subject, modMessage);
+        }
+        catch (IOException e)
+        {
+            LOGGER.throwing("sendRoles()", e);
+        }
+    }
+
+    private void newRound(boolean fresh)
+    {
+        round = round + 1;
+
+        if (round == 5)
+        {
+            endGame(fresh, false);
+            return;
+        }
+
+        List<Long> deck = (List<Long>)game.getDataProperty("round" + round + "deck");
+
+        int pl = 0, ind = 0;
+        int cards = 6 - round;
+        String cardName;
+        hands[0] = new String[cards];
+        for (long card : deck)
+        {
+            if (card == -1)
+                cardName = "boom";
+            else if (card <= success)
+                cardName = "safe";
+            else
+                cardName = "success";
+
+            hands[pl][ind++] = cardName;
+            if (ind == cards)
+            {
+                hands[++pl] = new String[cards];
+                ind = 0;
+            }
+        }
+
+        if (fresh)
+            sendHands();
+
+        turn = round - 1;
     }
 
     @Override
     public void initialize(boolean fresh)
     {
+        hands = new String[NoP][];
+        success = 0;
+        safe = 0;
+        round = 0;
+
+        if (fresh)
+            sendRoles();
+
+        newRound(fresh);
     }
 
     @Override
     protected void update()
     {
+        String message = getCurrentStatus() + "\n\n";
+        message += "[color=purple][b]" + players[turn] + " is up.[/b][/color]\n";
+        message += "[color=#008800]Please [b]choose [i]player[/i] [i]position[/i][/b][/color]";
+
+        try
+        {
+            helper.replyThread(game.getThread(), null, message);
+        }
+        catch (IOException e)
+        {
+            LOGGER.throwing("update()", e);
+        }
+    }
+
+    private void endGame(boolean fresh, boolean goodWin)
+    {
+        if (fresh)
+        {
+            String message = "[color=purple][b]The game is over. Team " + (goodWin ? "Good" : "Evil") + " (";
+            List<String> winners = new LinkedList<String>();
+            List<String> roles = (List<String>)game.getDataProperty("roles");
+            for (int i = 0; i < NoP; i++)
+            {
+                if (roles.get(i).equals("good") == goodWin)
+                    winners.add(players[i]);
+            }
+            message += Utils.join(winners, ", ") + ") wins![/b][/color]";
+            try
+            {
+                helper.replyThread(game.getThread(), null, message);
+            }
+            catch (IOException e)
+            {
+                LOGGER.throwing("endGame()", e);
+            }
+        }
+        super.endGame();
     }
 
     @Override
     protected void processMove(boolean fresh, String... move)
     {
+        if (move[0].equals("snip"))
+        {
+            int pl = Integer.parseInt(move[1]);
+            int ind = Integer.parseInt(move[2]);
+            String card = hands[pl][ind];
+            String message = null;
+
+            hands[pl][ind] = "drawn";
+
+            if (card.equals("success"))
+            {
+                message = "[color=green]" + players[turn] + "has drawn a [b]g{SUCCESS}g[/b] card![/color]";
+                success++;
+            }
+            else if (card.equals("safe"))
+            {
+                message = "[color=green]" + players[turn] + "has drawn a [b]b{SAFE}b[/b] card.[/color]";
+                safe++;
+            }
+            else if (card.equals("boom"))
+            {
+                message = "[color=green]" + players[turn] + "has drawn a [b][i]r{BOOM}r[/i][/b] card![/color]";
+            }
+            else
+            {
+                LOGGER.warning("Unrecognized card '%s'", card);
+            }
+
+            if (fresh)
+            {
+                try
+                {
+                    helper.replyThread(game.getThread(), null, message);
+                }
+                catch (IOException e)
+                {
+                    LOGGER.throwing("processMove()", e);
+                }
+            }
+
+            if (card.equals("boom"))
+                endGame(fresh, false);
+            else if (success == NoP)
+                endGame(fresh, true);
+            else if ((success + safe) % NoP == 0)
+                turn = pl;
+            else
+                newRound(fresh);
+        }
+        else
+        {
+            LOGGER.warning("Invalid move string '%s'", Utils.join(move, " "));
+        }
     }
 
     @Override
     public void processCommand(String username, String command)
     {
+        String name, position;
+        int index;
+        if (command.startsWith("choose "))
+        {
+            if (!username.equals(players[turn]))
+                return;
+
+            index = command.lastIndexOf(" ");
+            if (index < 7)
+            {
+                LOGGER.info("%s posted invalid choose command '%s'", players[turn], command);
+                return;
+            }
+            name = command.substring(7, command.lastIndexOf(" "));
+            position = command.substring(command.lastIndexOf(" "));
+
+            int pl = Utils.getUser(name, players);
+            if (pl == -1)
+            {
+                LOGGER.info("%s posted unrecognized username '%s'", players[turn], name);
+                return;
+            }
+            else if (pl == turn)
+            {
+                LOGGER.info("%s attempted to choose their own card", players[turn]);
+                return;
+            }
+
+            int pos = -1;
+            try
+            {
+                pos = Integer.parseInt(position) - 1;
+                if (pos < 0 || pos >= hands[pl].length)
+                {
+                    LOGGER.info("%s posted invalid position '%s'", players[turn], position);
+                    return;
+                }
+            }
+            catch (NumberFormatException e)
+            {
+                LOGGER.info("%s posted invalid position '%s'", players[turn], position);
+                return;
+            }
+
+            if (hands[pl][pos].equals("drawn"))
+            {
+                LOGGER.info("%s attempted to draw an already-drawn card", players[turn]);
+                return;
+            }
+
+            processAndAddMove("snip", Integer.toString(pl), Integer.toString(pos));
+        }
     }
 
     @Override
     public String getCurrentStatus()
     {
-        return null;
+        String message = "[color=#008800]";
+        if (success + safe > 0)
+        {
+            message += "[u]Cards drawn:[/u]\n";
+            if (success > 0)
+                message += success + " x g{SUCCESS}g\n";
+            if (safe > 0)
+                message += safe + " x b{SAFE}b\n";
+        }
+
+        message += "[u]Cards remaining:[/u][c]";
+
+        for (int i = 0; i < NoP; i++)
+        {
+            message += String.format("\n%-15s", players[i]);
+            for (int j = 0; j < hands.length; j++)
+            {
+                if (hands[i][j].equals("drawn"))
+                    message += " -";
+                else
+                    message += " " + (j + 1);
+            }
+        }
+        message += "[/c]";
+        return message;
     }
 
     @Override
