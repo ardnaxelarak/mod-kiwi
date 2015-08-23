f9a8493053b252f60bbf8b410510fc5a477a4c7f
Kara Felix <karairenefelix@gmail.com>
2015-08-23 03:10:15 -0400

    Utils.java: added left-padding and user recognition functions

--- src/main/java/modkiwi/util/Utils.java
+++ src/main/java/modkiwi/util/Utils.java
@@ -1,5 +1,6 @@
 package modkiwi.util;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public final class Utils
@@ -36,5 +37,34 @@ public final class Utils
             result += joiner + it.next().toString();
 
         return result;
+    }
+
+    public static String lPad(String text, int len)
+    {
+        return String.format("%-" + len + "s", text);
+    }
+
+    public static String lPadUsername(String name)
+    {
+        final int MAX_USERNAME_LENGTH = 15;
+        return lPad(name, MAX_USERNAME_LENGTH);
+    }
+
+    public static int getUser(String username, String[] list)
+    {
+        return getUser(username, Arrays.asList(list));
+    }
+
+    public static int getUser(String username, Iterable<String> list)
+    {
+        int index = 0;
+        for (String user : list)
+        {
+            if (username.equals(user))
+                return index;
+
+            index++;
+        }
+        return -1;
     }
 }
